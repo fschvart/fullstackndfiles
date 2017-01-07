1dedec960d0c95862ba4af3d76bf72d246ce33f5
Fabian Schvartzman <fabiansc@walla.com>
2017-01-07 01:38:14 -0800

    Add files via upload

---
+++ flask.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 03:32:21 2016
+
+@author: Fabian Schvartzman
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello')
+def HelloWorld() :
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host='0.0.0.0', port=5000)
